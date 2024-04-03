f16ebc8dfec75d9e92862a3cf85b8f1f97271202
De6ris <2838369930@qq.com>
2024-04-03 23:39:08 +0800

    把音效改坏了, 恢复原状
    另外鲨了一个Overwrite

--- src/main/java/net/oilcake/mitelros/mixins/SoundsMITEMixin.java
+++ src/main/java/net/oilcake/mitelros/mixins/SoundsMITEMixin.java
@@ -15,70 +15,28 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({SoundsMITE.class})
+@Mixin(SoundsMITE.class)
 public abstract class SoundsMITEMixin {// TODO
-
-    @Shadow protected abstract void loadMITESound(String path);
-
-    @Shadow public boolean loaded;
     @Shadow
     private List<String> sounds = new ArrayList<>();
 
 
-//    @ModifyExpressionValue(method = "load", at = @At(value = "INVOKE", target = "Lnet/minecraft/ResourcePack;resourceExists(Lnet/minecraft/ResourceLocation;)Z"))
-//    private boolean inject(boolean original) {
-//        return original || ;
-//    }
-
-    @Inject(method = "load", at = @At("HEAD"))
-    private void inject(CallbackInfoReturnable<Boolean> cir) {
-        if (!this.loaded) {
-            this.loadMITESound("records/damnation.ogg");
-            this.loadMITESound("records/connected.ogg");
-            this.loadMITESound("sound/random/totem_use.ogg");
-            this.loadMITESound("sound/meme/brainpower.ogg");
-            this.loadMITESound("sound/mob/spiderking/hit1.ogg");
-            this.loadMITESound("sound/mob/spiderking/hit1.ogg");
-            this.loadMITESound("sound/mob/spiderking/hit1.ogg");
-            this.loadMITESound("sound/mob/spiderking/hit1.ogg");
-            this.loadMITESound("sound/mob/spiderking/death.ogg");
-            this.loadMITESound("sound/mob/spiderking/say1.ogg");
-            this.loadMITESound("sound/mob/spiderking/say2.ogg");
-            this.loadMITESound("sound/mob/spiderking/say3.ogg");
-            this.loadMITESound("sound/random/melting.ogg");
-        }
+    @Inject(method = {"<init>(Lnet/minecraft/SoundManager;)V"}, at = {@At("RETURN")})
+    public void injectCtor(CallbackInfo callbackInfo) {
+        add("records/imported/damnation.ogg");
+        add("records/imported/connected.ogg");
+        add("sound/imported/random/totem_use.ogg");
+        add("sound/imported/meme/brainpower.ogg");
+        add("sound/imported/mob/spiderking/hit1.ogg");
+        add("sound/imported/mob/spiderking/hit1.ogg");
+        add("sound/imported/mob/spiderking/hit1.ogg");
+        add("sound/imported/mob/spiderking/hit1.ogg");
+        add("sound/imported/mob/spiderking/death.ogg");
+        add("sound/imported/mob/spiderking/say1.ogg");
+        add("sound/imported/mob/spiderking/say2.ogg");
+        add("sound/imported/mob/spiderking/say3.ogg");
+        add("sound/imported/random/melting.ogg");
     }
-
-
-//    @Inject(method = {"<init>(Lnet/minecraft/SoundManager;)V"}, at = {@At("RETURN")})
-//    public void injectCtor(CallbackInfo callbackInfo) {
-//        add("records/damnation.ogg");
-//        add("records/connected.ogg");
-//        add("sound/random/totem_use.ogg");
-//        add("sound/meme/brainpower.ogg");
-//        add("sound/mob/spiderking/hit1.ogg");
-//        add("sound/mob/spiderking/hit1.ogg");
-//        add("sound/mob/spiderking/hit1.ogg");
-//        add("sound/mob/spiderking/hit1.ogg");
-//        add("sound/mob/spiderking/death.ogg");
-//        add("sound/mob/spiderking/say1.ogg");
-//        add("sound/mob/spiderking/say2.ogg");
-//        add("sound/mob/spiderking/say3.ogg");
-//        add("sound/random/melting.ogg");
-////        add("records/imported/damnation.ogg");
-////        add("records/imported/connected.ogg");
-////        add("sound/imported/random/totem_use.ogg");
-////        add("sound/imported/meme/brainpower.ogg");
-////        add("sound/imported/mob/spiderking/hit1.ogg");
-////        add("sound/imported/mob/spiderking/hit1.ogg");
-////        add("sound/imported/mob/spiderking/hit1.ogg");
-////        add("sound/imported/mob/spiderking/hit1.ogg");
-////        add("sound/imported/mob/spiderking/death.ogg");
-////        add("sound/imported/mob/spiderking/say1.ogg");
-////        add("sound/imported/mob/spiderking/say2.ogg");
-////        add("sound/imported/mob/spiderking/say3.ogg");
-////        add("sound/imported/random/melting.ogg");
-//    }
 
     @Shadow
     private boolean add(String path) {
--- src/main/java/net/oilcake/mitelros/mixins/item/ItemBowMixin.java
+++ src/main/java/net/oilcake/mitelros/mixins/item/ItemBowMixin.java
@@ -6,9 +6,7 @@ import java.util.Random;
 import net.minecraft.DamageSource;
 import net.minecraft.Enchantment;
 import net.minecraft.EnchantmentHelper;
-import net.minecraft.Entity;
 import net.minecraft.EntityArrow;
-import net.minecraft.EntityLivingBase;
 import net.minecraft.EntityPlayer;
 import net.minecraft.EnumChatFormatting;
 import net.minecraft.EnumSignal;
@@ -17,7 +15,6 @@ import net.minecraft.ItemArrow;
 import net.minecraft.ItemBow;
 import net.minecraft.ItemStack;
 import net.minecraft.Material;
-import net.minecraft.Packet;
 import net.minecraft.Packet85SimpleSignal;
 import net.minecraft.Slot;
 import net.minecraft.Translator;
@@ -25,6 +22,7 @@ import net.minecraft.World;
 import net.oilcake.mitelros.enchantment.Enchantments;
 import net.oilcake.mitelros.item.Items;
 import net.oilcake.mitelros.item.Materials;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -34,8 +32,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin({ItemBow.class})
+@Mixin(ItemBow.class)
 public class ItemBowMixin extends Item {
     @Final
     @Shadow
@@ -53,7 +52,7 @@ public class ItemBowMixin extends Item {
     private static void injectClinit(CallbackInfo callback) {
         possible_arrow_materials = new Material[]{
                 Material.flint, Material.obsidian, Material.copper, Material.silver, Material.rusted_iron, Material.gold, Material.iron, Material.mithril, Material.adamantium, Material.ancient_metal,
-                (Material) Materials.nickel, (Material) Materials.tungsten, (Material) Materials.magical};
+                Materials.nickel, Materials.tungsten, Materials.magical};
     }
 
     @Overwrite
@@ -74,67 +73,47 @@ public class ItemBowMixin extends Item {
 
     @Overwrite
     public boolean onItemRightClick(EntityPlayer player, float partial_tick, boolean ctrl_is_down) {
-        if (!player.inCreativeMode() && player.inventory.getReadiedArrow() == null && !EnchantmentHelper.hasEnchantment(player.getHeldItemStack(), Enchantments.enchantmentInfinity))
+        if (!player.inCreativeMode() && player.inventory.getReadiedArrow() == null && !EnchantmentHelper.hasEnchantment(player.getHeldItemStack(), Enchantments.enchantmentInfinity)) {
             return false;
-        player.nocked_arrow = player.inventory.getReadiedArrow();
-        if ((player.nocked_arrow == null && player.inCreativeMode()) || EnchantmentHelper.hasEnchantment(player.getHeldItemStack(), Enchantments.enchantmentInfinity))
-            player.nocked_arrow = Items.arrowMagical;
-        if (player.onServer())
-            player.sendPacketToAssociatedPlayers((Packet) (new Packet85SimpleSignal(EnumSignal.nocked_arrow)).setShort(player.nocked_arrow.itemID).setEntityID((Entity) player), false);
-        player.setHeldItemInUse();
+        } else {
+            player.nocked_arrow = player.inventory.getReadiedArrow();
+            if ((player.nocked_arrow == null && player.inCreativeMode()) || EnchantmentHelper.hasEnchantment(player.getHeldItemStack(), Enchantments.enchantmentInfinity))
+                player.nocked_arrow = Items.arrowMagical;
+            if (player.onServer())
+                player.sendPacketToAssociatedPlayers((new Packet85SimpleSignal(EnumSignal.nocked_arrow)).setShort(player.nocked_arrow.itemID).setEntityID(player), false);
+            player.setHeldItemInUse();
+            return true;
+        }
+    }
+
+    @Redirect(method = "onPlayerStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/EntityPlayer;inCreativeMode()Z"))
+    private boolean inject(EntityPlayer instance) {
         return true;
     }
 
-    @Overwrite
-    public void onPlayerStoppedUsing(ItemStack item_stack, World world, EntityPlayer player, int item_in_use_count) {
-        if (!world.isRemote) {
-            ItemArrow arrow = player.inventory.getReadiedArrow();
-            if (arrow == null)
-                arrow = player.nocked_arrow;
-            float fraction_pulled = ItemBow.getFractionPulled(item_stack, item_in_use_count);
-            fraction_pulled = (fraction_pulled * fraction_pulled + fraction_pulled * 2.0F) / 3.0F;
-            if (fraction_pulled >= 0.1F) {
-                if (fraction_pulled > 1.0F)
-                    fraction_pulled = 1.0F;
-                EntityArrow entity_arrow = new EntityArrow(world, (EntityLivingBase) player, fraction_pulled * 2.0F, arrow, item_stack.isItemEnchanted());
-                player.nocked_arrow = null;
-                if (fraction_pulled == 1.0F)
-                    entity_arrow.setIsCritical(true);
-                Material material = item_stack.getMaterialForRepairs();
-                if (material == Materials.tungsten) {
-                    entity_arrow.setDamage(entity_arrow.getDamage() * 1.149999976158142D);
-                } else if (material == Material.mithril) {
-                    entity_arrow.setDamage(entity_arrow.getDamage() * 1.100000023841858D);
-                } else if (material == Material.ancient_metal) {
-                    entity_arrow.setDamage(entity_arrow.getDamage() * 1.0499999523162842D);
-                } else {
-                    entity_arrow.setDamage(entity_arrow.getDamage() * 0.75D);
-                }
-                int power = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, item_stack);
-                if (power > 0)
-                    entity_arrow.setDamage(entity_arrow.getDamage() + (power * 0.5F) + 0.5D);
-                int punch = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, item_stack);
-                if (punch > 0)
-                    entity_arrow.setKnockbackStrength(punch);
-                if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, item_stack) > 0)
-                    entity_arrow.setFire(100);
-                player.tryDamageHeldItem(DamageSource.generic, 1);
-                Random rand = new Random();
-                world.playSoundAtEntity((Entity) player, "random.bow", 1.0F, 1.0F / (rand.nextFloat() * 0.4F + 1.2F) + fraction_pulled * 0.5F);
-                if (player.inCreativeMode()) {
-                    entity_arrow.canBePickedUp = 2;
-                } else {
-                    player.inventory.consumeArrow();
-                }
-                if (!world.isRemote)
-                    world.spawnEntityInWorld((Entity) entity_arrow);
-            }
+    @Inject(method = "onPlayerStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/EntityPlayer;tryDamageHeldItem(Lnet/minecraft/DamageSource;I)Lnet/minecraft/ItemDamageResult;"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void modifyDamage(ItemStack item_stack, World world, EntityPlayer player, int item_in_use_count, CallbackInfo ci, ItemArrow arrow, float fraction_pulled, EntityArrow entity_arrow, int power, int punch) {
+        if (power > 0) {
+            entity_arrow.setDamage(entity_arrow.getDamage() - (double) ((float) power * 0.5F) - 0.5);
+        }
+        Material material = item_stack.getMaterialForRepairs();
+        if (material == Materials.tungsten) {
+            entity_arrow.setDamage(entity_arrow.getDamage() * 1.149999976158142D);
+        } else if (material == Material.mithril) {
+            entity_arrow.setDamage(entity_arrow.getDamage() * 1.100000023841858D);
+        } else if (material == Material.ancient_metal) {
+            entity_arrow.setDamage(entity_arrow.getDamage() * 1.0499999523162842D);
+        } else {
+            entity_arrow.setDamage(entity_arrow.getDamage() * 0.75D);
+        }
+        if (power > 0) {
+            entity_arrow.setDamage(entity_arrow.getDamage() + (double) ((float) power * 0.5F) + 0.5);
         }
     }
 
     @Inject(method = {"<init>(ILnet/minecraft/Material;)V"}, at = {@At("RETURN")})
     private void injectInit(CallbackInfo callbackInfo) {
-        setMaxDamage((this.reinforcement_material == Materials.tungsten) ? 256 : ((this.reinforcement_material == Material.mithril) ? 128 : ((this.reinforcement_material == Material.ancient_metal) ? 64 : 32)));
+        this.setMaxDamage((this.reinforcement_material == Materials.tungsten) ? 256 : ((this.reinforcement_material == Material.mithril) ? 128 : ((this.reinforcement_material == Material.ancient_metal) ? 64 : 32)));
     }
 
     /**
